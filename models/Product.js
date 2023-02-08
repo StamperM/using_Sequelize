ab7ce293b669d9a3a8fdeec7662dd1a116c00e5a
Melissa Gillen <melissagillen@Melissas-MacBook-Pro.local>
2023-02-08 08:40:46 -0500

    updated models

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER, STRING, DECIMAL } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,7 +9,36 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+
+    },
+    product_name: {
+      type: STRING,
+      allowNull: false,
+
+    },
+    price: {
+      type: DECIMAL,
+      allowNull:false,
+      isDecimal: true,
+
+    },
+    stock:{
+      type: INTEGER,
+      allowNull: false,
+      isNumeric:true,
+      equals:10,
+
+
+    },
+    category_id: {
+      type: INTEGER,
+      
+    }
   },
   {
     sequelize,
